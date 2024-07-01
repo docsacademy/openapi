5ae67547b74f66d61ce9abb7e3a5d730d73c6fbc
docsacademy <121296584+docsacademy@users.noreply.github.com>
2024-07-01 19:15:28 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "fax4a-api.json",
+    url: "fax4a-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
